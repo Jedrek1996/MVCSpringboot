93efc06745f618603670690fb5db08db0983ab50
Jed <kohjedrek@yahoo.com.sg>
2023-09-30 23:03:16 +0800

    First Commit

---
+++ src/main/java/com/jedrek/Main.java
@@ -0,0 +1,24 @@
+package com.jedrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+    @GetMapping("/greet")
+    public GreetRepsonse greet() {
+        return new GreetRepsonse("Hello");
+    }
+
+    record GreetRepsonse(String greet){}
+}
